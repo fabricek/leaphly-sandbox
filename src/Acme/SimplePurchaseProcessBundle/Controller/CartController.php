f97114f7bd24a873f2f0090bcfddef3f904beadb
fabricek <fabrice@actacenter.com>
2014-04-09 13:12:52 +0200

    CartController issue #13

--- src/Acme/SimplePurchaseProcessBundle/Controller/CartController.php
+++ src/Acme/SimplePurchaseProcessBundle/Controller/CartController.php
@@ -13,27 +13,28 @@ class CartController extends Controller
 
     public function playAction()
     {
+
         $conference = $this->get('doctrine_mongodb')
             ->getRepository('AcmeConferenceBundle:TicketProduct')
-            ->findBy(array(), null, $limit = 1)
-            ->getSingleResult();
+            ->findBy(array(), null, $limit = 1);
+            //->getSingleResult();
 
-        $ticketForm = $this->createForm(new TicketItemType($conference->getId()));
+        $ticketForm = $this->createForm(new TicketItemType($conference[0]->getId()));
 
         $productShirt = $this->get('doctrine_mongodb')
             ->getRepository('AcmeTshirtBundle:TshirtProduct')
-            ->findBy(array(), null, $limit = 1)
-            ->getSingleResult();
+            ->findBy(array(), null, $limit = 1);
+            //->getSingleResult();
 
-        $shirtForm = $this->createForm(new TshirtItemType($productShirt->getId()));
+        $shirtForm = $this->createForm(new TshirtItemType($productShirt[0]->getId()));
 
         return $this->render(
             'SimplePurchaseProcessBundle:Cart:play.html.twig',
             array(
                 'tshirt_form' => $shirtForm->createView(),
-                'tshirt_product' => $productShirt,
+                'tshirt_product' => $productShirt[0],
                 'ticket_form' => $ticketForm->createView(),
-                'ticket_product' => $conference)
+                'ticket_product' => $conference[0])
         );
     }
 }
